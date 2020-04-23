d350d4da50fc386d206d5f511e8506936dc879fd
adb203 <arianbaoas@gmail.com>
2020-04-23 15:44:24 -0400

    Updates

---
+++ precache-manifest.7a1c372e483d8a2fe23d62096642d390.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad821ea1a4a2b38faddbb0ccbc92f64",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "7975658ae09d2fe6e002",
+    "url": "/myportfolio/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "c7490bca4af799985def",
+    "url": "/myportfolio/static/css/main.6f26a851.chunk.css"
+  },
+  {
+    "revision": "7975658ae09d2fe6e002",
+    "url": "/myportfolio/static/js/2.84dfac3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.84dfac3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7490bca4af799985def",
+    "url": "/myportfolio/static/js/main.3997ee39.chunk.js"
+  },
+  {
+    "revision": "3f5dfdc1e91f975c6953",
+    "url": "/myportfolio/static/js/runtime-main.1cee7309.js"
+  },
+  {
+    "revision": "7e1dc720e8c6bbfccb9aad3abcf9a71b",
+    "url": "/myportfolio/static/media/3.7e1dc720.jpg"
+  },
+  {
+    "revision": "c09a40cc8bb9e07e9175255807ec2eb3",
+    "url": "/myportfolio/static/media/image1.c09a40cc.PNG"
+  },
+  {
+    "revision": "6e09f05a9a00db823b789d73b8d6d707",
+    "url": "/myportfolio/static/media/image2.6e09f05a.jpg"
+  },
+  {
+    "revision": "7e38092d4ecefb53230f9e304fd0bb86",
+    "url": "/myportfolio/static/media/resumev2.7e38092d.pdf"
+  },
+  {
+    "revision": "ffd5a22968810d0a72d00a6397d59d33",
+    "url": "/myportfolio/static/media/sudoku.ffd5a229.mp4"
+  }
+]);
